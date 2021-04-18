51eb994ffd822ad2d90da6152fdef18cf47bc033
paradoxials <70249884+paradoxials@users.noreply.github.com>
2021-04-18 17:46:29 +0800

    Users Register and login

--- GoRent_proj/GoRent_proj/urls.py
+++ GoRent_proj/GoRent_proj/urls.py
@@ -1,4 +1,4 @@
-"""GoRent_proj URL Configuration
+"""SoftwareRegisters URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.conf import settings
+from  django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+   	path('registers/', include('registers.urls', namespace='registers')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
