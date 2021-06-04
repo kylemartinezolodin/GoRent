0ab0e9f0a661a87864cf4b05c3a641994bc02ed4
Kyle Olodin <kylemartinezolodin@gmail.com>
2021-06-05 01:30:19 +0800

    Minor Fix from previous commit

--- GoRent_proj/GoRent_proj/settings.py
+++ GoRent_proj/GoRent_proj/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'GoRent_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 '''
+# DEFAULT
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
