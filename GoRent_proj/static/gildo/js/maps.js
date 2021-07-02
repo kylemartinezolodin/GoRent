c00f814acc6541bc09fc980b8650f4ae8819216c
Kyle Olodin <kylemartinezolodin@gmail.com>
2021-07-02 12:31:03 +0800

    First Fornt-End overhaul Prioritized changing the landing page for rentee users, which should handle the listing, saerch and detail display
    Changes to be committed:
    	modified:   GoRent_proj/GoRent_proj/urls.py
    	renamed:    GoRent_proj/gildo/templates/gildo/Renting.html -> GoRent_proj/gildo/templates/gildo/search.html
    	modified:   GoRent_proj/gildo/urls.py
    	modified:   GoRent_proj/gildo/views.py
    	new file:   GoRent_proj/static/css/leaflet/L.Control.MousePosition.css
    	modified:   GoRent_proj/static/gildo/css/tooplate-style.css
    	modified:   GoRent_proj/static/gildo/js/maps.js
    	new file:   GoRent_proj/static/img/Thumbs.db
    	new file:   GoRent_proj/static/img/bg-img-1.jpg
    	new file:   GoRent_proj/static/img/bg-img-2.jpg
    	new file:   GoRent_proj/static/img/bg-img-3.jpg
    	new file:   GoRent_proj/static/img/cebu-city.jpg
    	new file:   GoRent_proj/static/img/gorent_complete.png
    	new file:   GoRent_proj/static/img/gorent_icon.png
    	new file:   GoRent_proj/static/img/gorent_plain.png
    	new file:   GoRent_proj/static/img/gorent_text.png
    	new file:   GoRent_proj/static/img/img-01.jpg
    	new file:   GoRent_proj/static/img/img-02.jpg
    	new file:   GoRent_proj/static/img/img-03.jpg
    	new file:   GoRent_proj/static/img/img-04.jpg
    	new file:   GoRent_proj/static/img/img-05.jpg
    	new file:   GoRent_proj/static/img/logo.png
    	new file:   GoRent_proj/static/img/room-1.jpg
    	new file:   GoRent_proj/static/img/search.png
    	new file:   GoRent_proj/static/img/tn-img-01.jpg
    	new file:   GoRent_proj/static/img/tn-img-02.jpg
    	new file:   GoRent_proj/static/img/tn-img-03.jpg
    	new file:   GoRent_proj/static/img/tn-img-04.jpg
    	new file:   GoRent_proj/static/search/css/gorent_styles.css

--- GoRent_proj/static/gildo/js/maps.js
+++ GoRent_proj/static/gildo/js/maps.js
@@ -1,21 +1,83 @@
+var latitude = 10.3157
+var longitude = 123.8854
+var zoom_level = 10
+// function initMap() {
+//     const cebu = {lat: 10.3157, lng: 123.8854};
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center: cebu,
+//     });
+//     new google.maps.Marker({
+//         position: cebu,
+//         map,
+//         title: "Hello World!",
+//     });
+// }
 
+// MAP CODE
+map = L.map('map').setView([latitude, longitude], zoom_level); // ([LONG, LAT], ZOOM-LEVEL)
 
+// LAYER CODE (MURAG NI KATUNG SATELITE, ROAD, ETC. SA GOOGLE)
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmxhY2tsaXN0NjI2OTkiLCJhIjoiY2txZ2ZvY3llMDRqdjJwbnk0cW8xZHRwbiJ9.pEY5vLDBVfKplzQe92sMMw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYmxhY2tsaXN0NjI2OTkiLCJhIjoiY2txZ2ZvY3llMDRqdjJwbnk0cW8xZHRwbiJ9.pEY5vLDBVfKplzQe92sMMw'
+}).addTo(map);
 
+// MARKER CODE
+L.marker([latitude, longitude]).addTo(map)
 
-function initMap(){
-
-    var options = {
-        center: {lat: 10.3157, lng: 123.8854},
-        zoom: 8
+L.Control.MousePosition = L.Control.extend({
+    options: {
+      position: 'bottomleft',
+      separator: ' : ',
+      emptyString: 'Unavailable',
+      lngFirst: false,
+      numDigits: 5,
+      lngFormatter: undefined,
+      latFormatter: undefined,
+      prefix: ""
+    },
+  
+    onAdd: function (map) {
+      this._container = L.DomUtil.create('div', 'leaflet-control-mouseposition');
+      L.DomEvent.disableClickPropagation(this._container);
+      map.on('mousemove', this._onMouseMove, this);
+      this._container.innerHTML=this.options.emptyString;
+      return this._container;
+    },
+  
+    onRemove: function (map) {
+      map.off('mousemove', this._onMouseMove)
+    },
+  
+    _onMouseMove: function (e) {
+      var lng = this.options.lngFormatter ? this.options.lngFormatter(e.latlng.lng) : L.Util.formatNum(e.latlng.lng, this.options.numDigits);
+      var lat = this.options.latFormatter ? this.options.latFormatter(e.latlng.lat) : L.Util.formatNum(e.latlng.lat, this.options.numDigits);
+      var value = this.options.lngFirst ? lng + this.options.separator + lat : lat + this.options.separator + lng;
+      var prefixAndValue = this.options.prefix + ' ' + value;
+      this._container.innerHTML = prefixAndValue;
     }
+  
+  });
+  
+  L.Map.mergeOptions({
+      positionControl: false
+  });
+  
+  L.Map.addInitHook(function () {
+      if (this.options.positionControl) {
+          this.positionControl = new L.Control.MousePosition();
+          this.addControl(this.positionControl);
+      }
+  });
+  
+  L.control.mousePosition = function (options) {
+      return new L.Control.MousePosition(options);
+  };
 
-    // new map
-    map = new google.maps.Map(document.getElementById("map"), options)
-
-    // var marker = new google.maps.Marker({
-    //     position: {lat: 10.2956, lang: 123.8805},
-    //     map:map
-    // }
-
-    
-}
+  
+L.control.mousePosition().addTo(map);
