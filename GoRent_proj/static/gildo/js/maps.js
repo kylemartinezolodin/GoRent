c64249f26f09b8d07a583eb23f2dccd61b3c5c81
Kyle Olodin <kylemartinezolodin@gmail.com>
2021-07-05 01:03:57 +0800

    Nearby list added with functionalities The right-side panel now has functions to locate nearby spaces either manully clicking or automatic search. Together with this commit is a working forward geosearch for updates map viewport when searching an address.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   GoRent_proj/gildo/templates/gildo/search.html
    	new file:   GoRent_proj/static/css/leaflet/geosearch.css
    	modified:   GoRent_proj/static/gildo/css/tooplate-style.css
    	modified:   GoRent_proj/static/gildo/js/maps.js
    	new file:   GoRent_proj/static/js/leaflet/geosearch.js
    	new file:   GoRent_proj/static/js/leaflet/leaflet-geoip.js
    	new file:   GoRent_proj/static/search/css/map_styles.css
    	renamed:    GoRent_proj/static/search/css/gorent_styles.css -> GoRent_proj/static/search/css/page_styles.css
    	renamed:    GoRent_proj/static/search/css/nearby_list.css -> GoRent_proj/static/search/css/sidepanel_left.css

--- GoRent_proj/static/gildo/js/maps.js
+++ GoRent_proj/static/gildo/js/maps.js
@@ -15,7 +15,12 @@ var zoom_level = 10
 // }
 
 // MAP CODE
-map = L.map('map').setView([latitude, longitude], zoom_level); // ([LONG, LAT], ZOOM-LEVEL)
+// map = L.map('map1').setView([latitude, longitude], zoom_level); // ([LONG, LAT], ZOOM-LEVEL)
+map = L.map('map', {
+  center: [latitude, longitude],
+  zoom: zoom_level,
+  zoomControl: false,
+});
 
 // LAYER CODE (MURAG NI KATUNG SATELITE, ROAD, ETC. SA GOOGLE)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmxhY2tsaXN0NjI2OTkiLCJhIjoiY2txZ2ZvY3llMDRqdjJwbnk0cW8xZHRwbiJ9.pEY5vLDBVfKplzQe92sMMw', {
@@ -30,6 +35,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // MARKER CODE
 L.marker([latitude, longitude]).addTo(map)
 
+// CURSOR LONGITUDED AND LATITUDE DISPLAY CODE
 L.Control.MousePosition = L.Control.extend({
     options: {
       position: 'bottomleft',
@@ -62,22 +68,138 @@ L.Control.MousePosition = L.Control.extend({
       this._container.innerHTML = prefixAndValue;
     }
   
-  });
-  
-  L.Map.mergeOptions({
-      positionControl: false
-  });
-  
-  L.Map.addInitHook(function () {
-      if (this.options.positionControl) {
-          this.positionControl = new L.Control.MousePosition();
-          this.addControl(this.positionControl);
-      }
-  });
-  
-  L.control.mousePosition = function (options) {
-      return new L.Control.MousePosition(options);
-  };
+});
 
-  
+L.Map.mergeOptions({
+    positionControl: false
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.positionControl) {
+        this.positionControl = new L.Control.MousePosition();
+        this.addControl(this.positionControl);
+    }
+});
+
+L.control.mousePosition = function (options) {
+    return new L.Control.MousePosition(options);
+};
+
+
 L.control.mousePosition().addTo(map);
+
+
+// GEOSEARCH
+geocoder = L.Control.Geocoder.nominatim()
+geosearch_control = L.Control.geocoder({
+  defaultMarkGeocode: false,
+  collapsed: false,
+  position: "topleft",
+  suggestTimeout: 100,
+  geocoder: geocoder,
+}).addTo(map);
+
+var search_boundingbox
+geosearch_control.on('markgeocode', function(e) { // BOUNDING-BOX
+  var bbox = e.geocode.bbox;
+  console.log(bbox);
+
+  if (search_boundingbox != null) {
+    search_boundingbox.remove()
+    search_boundingbox = L.polygon([
+      bbox.getSouthEast(),
+      bbox.getNorthEast(),
+      bbox.getNorthWest(),
+      bbox.getSouthWest()
+    ]).addTo(map);
+    map.fitBounds(search_boundingbox.getBounds());
+  }
+  else {
+    search_boundingbox = L.polygon([
+      bbox.getSouthEast(),
+      bbox.getNorthEast(),
+      bbox.getNorthWest(),
+      bbox.getSouthWest()
+    ]).addTo(map);
+    map.fitBounds(search_boundingbox.getBounds());
+  }
+
+  console.log(e);
+  addPopupFromResult(map, e.geocode.center, e.geocode)
+  user_locate.location = e.geocode.center
+});
+
+map.on('click', function(e) { // DISPLAY LOCATION ON CLICK
+  if (user_locate.isLocating) {
+    geocoder.reverse(e.latlng, map.options.crs.scale(map.getZoom()), function(results) {
+      result = results[0]; // THE "result" VARIABLE IS A geocode OBJECT
+      addPopupFromResult(map, e.latlng, result)
+
+      user_locate.location = e.latlng
+      toggleMapLocate()
+      
+      console.log(geolocate_marker.getLatLng())
+    });
+  }
+});
+
+
+function showLocation(position) {
+  position = L.latLng([position.coords.latitude, position.coords.longitude])
+  map.setView(position, 15)
+  geocoder.reverse(position, map.options.crs.scale(map.getZoom()), function(results) {
+    result = results[0]; // THE "result" VARIABLE IS A geocode OBJECT
+    addPopupFromResult(map, position, result, "You are here: <br>")
+
+    user_locate.location = position
+
+    console.log(geolocate_marker.getLatLng())
+  });
+}
+
+var geolocate_marker;
+function addPopupFromResult(map, latlng, result="", message="") { // DISPLAY LOCATION ON CLICK
+  if (result != null) {
+    if (geolocate_marker != null) {
+      geolocate_marker
+        .setLatLng(latlng)
+        .setPopupContent(message +(result.html || result.name))
+        .openPopup();
+    } else {
+      geolocate_marker = L.marker(latlng)
+        .bindPopup(message +result.name)
+        .addTo(map)
+        .openPopup();
+    }
+  }
+}
+
+
+
+
+// searchControl = new  GeoSearch.GeoSearchControl({
+//   provider: new  GeoSearch.OpenStreetMapProvider(),
+//   style: 'bar',
+//   // showMarker: true,
+//   // autoClose: true,
+//   // updateMap: true,
+// });
+
+// map.addControl(searchControl);
+
+
+// const provider = new GeoSearch.OpenStreetMapProvider();
+// const results = provider.search({ query: "spain" });
+// console.log(results);
+
+// const search = new GeoSearch.GeoSearchControl({
+//   provider: new GeoSearch.OpenStreetMapProvider(),
+// });
+// map.addControl(search);
+
+
+
+// const searchControl = new GeoSearchControl({
+//   provider: new OpenStreetMapProvider(),
+// });
+// map.addControl(searchControl);
