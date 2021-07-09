ff276abdb769774d35f978b60d1a1e65ccbe2065
Kyle Olodin <kylemartinezolodin@gmail.com>
2021-07-10 00:53:31 +0800

    Only RentOwner can "Add Space" is fixed
    Sharee users no longer have "Add Space" functionalities
    For this fix I it was nessecary to make some improvement to some urls.py and view.py
    
    Changes to be committed:
    	modified:   GoRent_proj/gildo/templates/gildo/search.html
    	modified:   GoRent_proj/gildo/views.py
    	modified:   GoRent_proj/registers/templates/registers/landingPage.html
    	modified:   GoRent_proj/registers/templates/registers/loginOwner.html
    	modified:   GoRent_proj/registers/templates/registers/loginSharee.html
    	modified:   GoRent_proj/registers/urls.py
    	modified:   GoRent_proj/registers/views.py

--- GoRent_proj/registers/urls.py
+++ GoRent_proj/registers/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 app_name='registers'
 urlpatterns=[
-   path('Owner/Login', views.GoRentLoginOwnerPage.as_view(), name="gorent_loginOwner_view"),
-   path('Sharee/Login', views.GoRentLoginShareePage.as_view(), name="gorent_loginSharee_view"),
-   path('Owner/Register', views.GoRentOwnerRegisterPage.as_view(), name="gorent_OwnerRegister_view"),
-   path('Sharee/Register', views.GoRentShareeRegisterPage.as_view(), name="gorent_ShareeRegister_view"),
+   path('Owner/Login', views.GoRentLoginOwnerPage.as_view(), name="loginOwner_view"),
+   path('Sharee/Login', views.GoRentLoginShareePage.as_view(), name="loginSharee_view"),
+   path('Owner/Register', views.GoRentOwnerRegisterPage.as_view(), name="OwnerRegister_view"),
+   path('Sharee/Register', views.GoRentShareeRegisterPage.as_view(), name="ShareeRegister_view"),
+   path('', views.GoRentAboutPage.as_view(), name="about_view"),
    path('Logout', views.GoRentLogoutPage.as_view(), name="logout_view"),
 ]
